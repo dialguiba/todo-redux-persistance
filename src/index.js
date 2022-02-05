c5752b2639cbf4ec9ecfe00f0266bc21000f4cb7
dialguiba <dialguiba@gmail.com>
2022-02-04 22:46:12 -0500

    (refactor) remove unused

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { store, persistor } from "./store/store";
 import { Provider } from "react-redux";
 /*  */
 import { PersistGate } from "redux-persist/integration/react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
